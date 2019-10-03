3e3cddf5f0f2ee1ae34ce8a9f1bff7c20bd275a5
Ubuntu <ubuntu@ip-172-31-16-122.ec2.internal>
2019-10-03 20:06:05 +0000

    testing

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     insecureAuth: true
 }).connect(function (err){
     if (err) throw err;
-    console.log("Connected to database");
+    console.log("Connected to database!");
 });
 
 
