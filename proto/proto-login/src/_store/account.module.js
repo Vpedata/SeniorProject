ffa3aac26f376a4cbb4847c7615bfc7a0d4c4c4f
Josiah Bell <36246936+JosiahBell@users.noreply.github.com>
2019-10-15 06:52:50 -0400

    Comments added

--- proto/proto-login/src/_store/account.module.js
+++ proto/proto-login/src/_store/account.module.js
@@ -1,12 +1,21 @@
+/**
+  * builds the 'account' constant that is exported
+  * imports 'userService' constant exported from the 'user.service.js' file in the '../_services' folder, exported by the 'index.js' file in the same folder
+  * imports 'router' constant exported from the 'router.js' file in the '../_helpers' folder, exported by the 'index.js' file in the same folder
+  */
 import { userService } from '../_services';
 import { router } from '../_helpers';
 
+//gets the account 'user' information by parsing JSON data for the 'user'
 const user = JSON.parse(localStorage.getItem('user'));
+//sets the user state based upon the user's status
 const state = user
-    ? { status: { loggedIn: true }, user }
-    : { status: {}, user: null };
+    ? { status: { loggedIn: true }, user }  //Logged in? Here is the user state
+    : { status: {}, user: null };           //Not logged in? The user state is null
 
+//sets the user actions of the account
 const actions = {
+    //login action (email and password = correct? login is successful, here is the user. email & password != correct? login fails)
     login({ dispatch, commit }, { email, password }) {
         commit('loginRequest', { email });
     
@@ -22,10 +31,12 @@ const actions = {
                 }
             );
     },
+    //logs user out
     logout({ commit }) {
         userService.logout();
         commit('logout');
     },
+    //register this as a new user as long as the register is successful
     register({ dispatch, commit }, user) {
         commit('registerRequest', user);
     
@@ -47,34 +58,43 @@ const actions = {
     }
 };
 
+//sets the user mutations in the account
 const mutations = {
+    //login request process
     loginRequest(state, user) {
         state.status = { loggingIn: true };
         state.user = user;
     },
+    //login successful process
     loginSuccess(state, user) {
         state.status = { loggedIn: true };
         state.user = user;
     },
+    //login failure process
     loginFailure(state) {
         state.status = {};
         state.user = null;
     },
+    //logout process
     logout(state) {
         state.status = {};
         state.user = null;
     },
+    //register request process
     registerRequest(state, user) {
         state.status = { registering: true };
     },
+    //register success process
     registerSuccess(state, user) {
         state.status = {};
     },
+    //register failure process
     registerFailure(state, error) {
         state.status = {};
     }
 };
 
+//exports the constant 'account' that holds all of the user's information (state, actions, & mutations)
 export const account = {
     namespaced: true,
     state,
