f00dcb180a59e2e3ac10e56788c803d82dd08f68
Josiah Bell <36246936+JosiahBell@users.noreply.github.com>
2019-10-15 06:13:23 -0400

    Comments added

--- proto/proto-login/src/plugins/vuetify.js
+++ proto/proto-login/src/plugins/vuetify.js
@@ -1,3 +1,8 @@
+/** 
+  * Sets up Vuetify library plugin, sets Vue to use Vuetify,
+  * and then exports a new Vuetifyinstance with the empty
+  * constant 'opts' json array.
+  */
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
