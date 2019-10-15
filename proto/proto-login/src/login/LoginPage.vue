788d3cfb74f5bd1c50d90367cdebf6dedf14b340
Josiah Bell <36246936+JosiahBell@users.noreply.github.com>
2019-10-15 06:23:14 -0400

    Comments added

--- proto/proto-login/src/login/LoginPage.vue
+++ proto/proto-login/src/login/LoginPage.vue
@@ -1,18 +1,21 @@
-<!-- Login page prototype vue code template -->
+<!-- Login page prototype vue code template (builds the login page) -->
 <template>
     <div>
         <h2>Login</h2>
         <form @submit.prevent="handleSubmit">
+            <!-- Email Form Group (Label & Textbox w/ Error Handling) -->
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="text" v-model="email" name="email" class="form-control" :class="{ 'is-invalid': submitted && !email }" />
                 <div v-show="submitted && !email" class="invalid-feedback">Email is required</div>
             </div>
+            <!-- Password Form Group (Label & Textbox w/ Error Handling) -->
             <div class="form-group">
                 <label htmlFor="password">Password</label>
                 <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
                 <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
             </div>
+            <!-- Login Button Form Group ("Login" Button w/ Logging In Gif, & Create Account w/ Link To Register Page) -->
             <div class="form-group">
                 <button class="btn btn-primary" :disabled="status.loggingIn">Login</button>
                 <img v-show="status.loggingIn" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
@@ -22,10 +25,13 @@
     </div>
 </template>
 
+<!-- Page Vue Script -->
 <script>
+//imports vuex state and action mapping functions
 import { mapState, mapActions } from 'vuex'
 
 export default {
+    //empty default data
     data () {
         return {
             email: '',
@@ -40,11 +46,13 @@ export default {
         // reset login status
         this.logout();
     },
+    //account login submission handling
     methods: {
         ...mapActions('account', ['login', 'logout']),
         handleSubmit (e) {
             this.submitted = true;
             const { email, password } = this;
+            //if email && password are true, save email and password for user state handling
             if (email && password) {
                 this.login({ email, password })
             }
