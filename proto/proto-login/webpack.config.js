8517104d8ad5b34bf0a7c1f4b8d3181eaae4e65d
jpk1101 <jpk.mymail@gmail.com>
2019-10-14 22:48:28 -0400

    Proto-login file
    Adding a login system with simulated backend to the prototype folder. I originally intended to merge it with the current prototype to allow both to be ran using the same packages, but different package versions made that seem impossible. To open the login page, go to the proto\proto-login directory and run 'npm start'.

---
+++ proto/proto-login/webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                fiber: require('fibers'),
+                                indentedSyntax: true // optional
+                            },
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.vue?$/,
+                exclude: /(node_modules)/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'www.onlineadvisor.tk:3000'
+        })
+    }
+}
