55792666cf42188bc737a65033c9bd8489fabde4
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-20 16:50:22 -0400

    Testing Oauth

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,23 +5,28 @@ var db = require ("./db.js");
 // getting the local authentication type
 const GoogleStrategy = require('passport-google-oauth20');
 
-passport.use(
-  new GoogleStrategy({
+passport.use(new GoogleStrategy({
     callbackURL:'/auth/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret,
     passReqToCallback   : true
-  }, (accessToken,refreshToken,profile,done) => {
+  }, 
+  (accessToken,refreshToken,profile,done) => {
       //google callback
-      let google_id = profile.id;
       var sql = "SELECT * FROM User where google_id= ?";
-	  db.query(sql,[google_id],(err, rows, fields)=>{
-        if (err) throw err;
-        if(!rows.length){
-            //new user
+	  db.query(sql,[profile.id],(err, rows, fields)=>{
+        if (err) return done(err);
+        if(rows.length)
+        {
+            done(null, rows[0]);
         }
-        else {
-            //existing user
+        else 
+        {
+            const User = {
+                googleId: profile.id,
+                username: profile.displayName,
+            }
+            done (null,User);
         }
-        
-  }));
+    });
+}));
