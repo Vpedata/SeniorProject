0dd987b0dbe0709f40de43896c119843f3d74479
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-22 09:16:12 -0400

    Rearraging the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
  
 // creating an express instance
-const app = express()
-
-const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
+const app = express()  
 const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+//const cookieSession = require('cookie-session');
 const session = require('express-session');
 
 //google OAuth2.0 using passport
 require('./config/passport.js');
 
+const publicRoot = '/home/ubuntu/SeniorProject/frontend/dist'
 
-app.use(cookieSession({
-    name: 'session',
-    keys: ['proto'],
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+app.use(express.static(publicRoot))
+app.use( cookieParser()); 
+app.use( bodyParser.json());
+app.use( bodyParser.urlencoded({extended: true}));
+
+//app.use(cookieSession({
+//    name: 'session',
+//    keys: ['proto'],
+//    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+//}))
 
 app.use(session({
     secret: 'keyboard cat',
@@ -34,12 +40,8 @@ const routes = require('./routes.js');
 //database connection
 var connection = require ("./config/db.js");
 
-const publicRoot = '/home/ubuntu/SeniorProject/frontend/dist'
-
-app.use(express.static(publicRoot))
 app.use(routes);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use (bodyParser.json());
+
 
 app.listen(3000, () => {
     console.log("proto  app listening on port 3000")
