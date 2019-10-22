86c012c63557bcb924c84d2e1f261fbbf856427e
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-22 01:24:37 -0400

    Changes to express session

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,12 @@ app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 
-app.use(session({secret: 'proto' }));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true } 
+}));
 
 app.use (passport.initialize());
 app.use (passport.session());
