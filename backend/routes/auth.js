be3f9bb73dbf84b9815acdd621d77f909f69c0a7
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-21 23:03:07 -0400

    Adding loggins for testing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,6 +10,7 @@ router.get('/login',passport.authenticate('google',{
 
 //callback route for google to redirect to
 router.get('/redirect',passport.authenticate('google',(req, res)=>{
+    // res.send(req.user);
     res.redirect('/');
 }));
     
