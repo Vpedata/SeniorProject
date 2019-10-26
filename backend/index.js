b759921818832f4933212f76fd1f6bb719aa560e
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-26 19:56:12 -0400

    Shifting cookie-session to express-session

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express()  
 const passport = require('passport');
 const bodyParser = require('body-parser');
-//const cookieParser = require('cookie-parser')
-const cookieSession = require('cookie-session');
-//const session = require('express-session');
+const cookieParser = require('cookie-parser')
+//const cookieSession = require('cookie-session');
+const session = require('express-session');
 
 //google OAuth2.0 using passport
 require('./config/passport.js');
@@ -14,22 +14,25 @@ require('./config/passport.js');
 const publicRoot = '/home/ubuntu/SeniorProject/frontend/dist'
 
 app.use(express.static(publicRoot))
-//app.use( cookieParser()); 
+app.use( cookieParser()); 
 app.use( bodyParser.json());
 app.use( bodyParser.urlencoded({extended: true}));
 
-app.use(cookieSession({
-    name: 'session',
-    keys: ['proto'],
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+//app.use(cookieSession({
+//    name: 'session',
+//    keys: ['proto'],
+//    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+//}))
 
-//app.use(session({
-//    secret: 'keyboard cat',
-//    resave: false,
-//    saveUninitialized: true,
-//    cookie: { secure: true } 
-//}));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+     } 
+}));
 
 app.use (passport.initialize());
 app.use (passport.session());
