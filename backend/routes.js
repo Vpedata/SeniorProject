0bb4bf3efbd5a8ce14af36b010ecbc7b83c36c98
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-27 15:01:27 -0400

    Adding restful api for courses

--- backend/routes.js
+++ backend/routes.js
@@ -3,8 +3,9 @@ const router = require('express').Router();
 bodyParser = require('body-parser');
 router.use(bodyParser.json()); 
 
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const courseRoutes = require('./routes/course');
 
 // display the index.html
 router.get("/", (req, res, next) => {
@@ -15,6 +16,9 @@ router.get("/", (req, res, next) => {
 router.use('/auth',authRoutes);
 
 // routes to user
+router.use('/user',userRoutes);
+
+// routes to course
 router.use('/user',userRoutes);
 
 module.exports = router;
