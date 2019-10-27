c95ea65d5fcdf3c2866cea259845444188f4eef1
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-10-26 21:14:03 -0400

    Adding console logs for debugging

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,7 +12,7 @@ const authMiddleware = (req, res, next) => {
 
 //User Homepage
 router.get("/",authMiddleware, (req, res, next)=>{
-    console.log(req.session.)
+    console.log(req.session);
     res.send(req.user);
 });
 
