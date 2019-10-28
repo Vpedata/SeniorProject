43403bef2130d8b629b74b2c7d96dc7ee77391b2
Eric Stephens <ericwstephens3@gmail.com>
2019-10-28 14:53:28 -0400

    trying to fix login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Login from './login/LoginPage'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -10,6 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: { Login },
   template: '<App/>'
 })
