f1d37d5bb378512a4c6cf2478b8319443ebe7849
Ubuntu <ubuntu@ip-172-31-57-35.ec2.internal>
2019-10-30 21:31:51 +0000

    working authentication for first page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,12 +7,6 @@
 <script>
 export default {
   name: 'App',
-  methods: {
-    logout: function(){
-        $http.get("http://onlineadvisor.tk:3000/auth/logout");      
-        $router.push('http://onlineadvisor.tk:3000/#/');
-    }
-},
 }
 </script>
 
--- frontend/src/login/UserPage.vue
+++ frontend/src/login/UserPage.vue
@@ -4,7 +4,7 @@
             <v-btn class="ma-2" outlined color="indigo" @click="$router.push('/user')" dark>Refresh</v-btn>
         </div>
         <div class="text-center">
-            <v-btn class="ma-2" outlined color="indigo" @click="logout" dark>To Login Page</v-btn>
+            <b-btn @click="logout()">To Login Page</b-btn>
         </div>
     </v-app>
 </template>
@@ -12,6 +12,12 @@
 <script>
 export default {
     name: 'Student',
+  methods: {
+    logout(){
+        $http.get("http://onlineadvisor.tk:3000/auth/logout");
+        $router.push('/');
+    }
+},
     data () {
     return {
       msg: 'Welcome to THe student Page'
