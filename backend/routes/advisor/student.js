30685ffcc6c12a9be8bdcdc9da8b39924533c207
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-02 10:49:22 -0400

    Adding advisor to view all students

--- backend/routes/advisor/student.js
+++ backend/routes/advisor/student.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const isAdvisor = require("./advisorAuth.js");
 
+//get all students for the current advisor 
+router.get("/all",isAdvisor, (req, res, next)=> {
+    res.send("Under Construction");
+});
+
 //get student's list of courses recommended and taken by student email 
 router.get("/:email",isAdvisor, (req, res, next)=> {
     res.send("Under Construction");
