8fbeb75f3d65b46c1613e84012e84f8d03c0c550
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-07 12:00:28 -0500

    Cleaning Student and Advsior Auth

--- backend/routes/advisor/advisorAuth.js
+++ backend/routes/advisor/advisorAuth.js
@@ -1,8 +1,13 @@
 //check if user is advisor only; 
 const isAdvisor= (req, res, next) => {
-    if (!req.user[0].IsStudent)
+    if(!req.user[0].IsStudent)
+    {
         return next();
-    res.redirect('/auth/login');
+    }
+    else 
+    {
+        res.redirect('http://onlineadvisor.tk:3000/#/');
+    }   
 };
 
 module.exports = isAdvisor; 
