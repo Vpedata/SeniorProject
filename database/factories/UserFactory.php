24a681a86d797e8a0494d511fd00d4b4c2756440
JosiahBell <bellj5@students.rowan.edu>
2019-11-08 10:40:10 -0500

    Minor fake backend cleanup and prep for future changes.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'phone' => $faker->phoneNumber,
         'email' => $faker->unique()->safeEmail,
+        'isStudent' => rand(0, 1),
         'profile_image' => 'http://via.placeholder.com/150x150',
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -31,7 +31,7 @@ $factory->define(User::class, function (Faker $faker) {
 $factory->define(App\Message::class, function (Faker $faker) {
    do {
        $from = rand(1, 15);
-       $to = rand(1,15);
+       $to = rand(1, 15);
    } while ($from === $to);
 
     return [
