394e7b51bcc4db3da445d42082e936f18a0e1d5a
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-10 09:33:14 -0500

    Adding routes to /student/courses and /student/messages

--- backend/routes/student/messages.js
+++ backend/routes/student/messages.js
@@ -3,13 +3,21 @@ const isStudent = require("./studentAuth.js");
 const db = require("../../config/db.js");
 
 //Return all the messages of the current student; 
-router.get("/all",isStudent, (req, res, next)=>{
-    res.send("Under construction");
+router.get("/all/:id",isStudent, (req, res, next)=>{
+    var sql = "CALL getAllChatsForStudent(?);";
+    db.query(sql,req.user.student_ID, (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    });
 });
 
 //Send a message to student's advisor; 
-router.post("/new",isStudent, (req, res, next) => {
-    res.send("Under construction");
+router.post("/:message",isStudent, (req, res, next) => {
+    sql = "CALL sendMessageAsStudent(?,?);"
+    db.query(sql,[req.params.message,req.user.student_ID], (err, rows, fields) => {
+        if (err) throw err;
+        res.send(rows);
+    });
 });
 
 //delete all the messages of the current student
