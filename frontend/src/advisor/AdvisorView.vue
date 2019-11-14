6925366ed5742ba3b36d089fc63b00b42e12e551
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-13 19:47:01 -0500

    Fixing logout and messages buttons

--- frontend/src/advisor/AdvisorView.vue
+++ frontend/src/advisor/AdvisorView.vue
@@ -66,10 +66,10 @@
             </v-row>
              <v-row>
                 <v-col cols="8">
-                    <v-btn class="mt-12 ma-12" outlined color="brown" @click="logout" dark>Logout</v-btn>
+                    <v-btn  @click="logout" dark>Logout</v-btn>
                 </v-col>
                 <v-col cols="4">
-                    <v-btn class="mt-12" outlined color="brown" @click="$router.push('/messages')" dark>Messages</v-btn>
+                    <v-btn  @click="$router.push('/messages')" dark>Messages</v-btn>
                 </v-col>
             </v-row>
         </div>
