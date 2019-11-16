9972ce04e7c167a15a039c8c3a31dec26d84011a
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-16 07:43:17 -0500

    Removing vue components from advisor page

--- frontend/src/advisor/AdvisorView.vue
+++ frontend/src/advisor/AdvisorView.vue
@@ -6,6 +6,8 @@
                 <v-toolbar color="amber darken-1" dark>
                 <v-toolbar-title class="brown--text">
                     {{name}}
+                    <v-btn  @click="$router.push('/messages')" dark>Messages</v-btn>
+                    <v-btn  @click="logout" dark>Logout</v-btn>
                 </v-toolbar-title>
                 </v-toolbar>
             </v-row>
@@ -20,58 +22,7 @@
                     :get-result-value="getResultValue" ></autocomplete>
                 </v-col>
             </v-row>
-            <v-row>
-                <v-col cols="3"></v-col>
-                <v-col cols="9" lg="6">
-                <v-card class="mt-n16 mx-auto" elevation="12" height="350px">
-                    <v-toolbar flat>
-                        <v-toolbar-title class="grey--text">View Courses for Students (List Students)</v-toolbar-title>
-                    </v-toolbar>
-                    <v-list style="max-height: 300px" class="overflow-y-auto">
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-dialog v-model="dialog" width="500">
-                                    <template v-slot:activator="{ on }">
-                                    <v-btn color="amber darken-1" dark v-on="on">
-                                        (Get Student Name)
-                                    </v-btn>
-                                    </template>
-                                    <v-card>
-                                        <v-card-title
-                                            class="headline grey lighten-2"
-                                            primary-title
-                                        >
-                                            (Get Student Name)
-                                        </v-card-title>
-                                        <v-divider></v-divider>
-                                        <v-card-text>
-                                            (Get Student's Planned Classes)
-                                        </v-card-text>
-                                        <v-divider></v-divider>
-                                        <v-card-actions>
-                                            <v-spacer></v-spacer>
-                                            <v-btn color="blue" text @click="dialog = false">
-                                                Ok
-                                            </v-btn>
-                                        </v-card-actions>
-                                    </v-card>
-                                </v-dialog>
-                            </v-list-item-content>
-                        </v-list-item>
-
-                    </v-list>
-                    
-                </v-card>
-                </v-col>
-  
-            </v-row>
              <v-row>
-                <v-col cols="8">
-                    <v-btn  @click="logout" dark>Logout</v-btn>
-                </v-col>
-                <v-col cols="4">
-                    <v-btn  @click="$router.push('/messages')" dark>Messages</v-btn>
-                </v-col>
             </v-row>
         </div>
     </v-app>
