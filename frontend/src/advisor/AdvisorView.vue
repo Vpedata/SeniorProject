6ecdc5f8054b619999b6839d058d2bfb751e59a3
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-17 16:05:31 -0500

    Changing url address

--- frontend/src/advisor/AdvisorView.vue
+++ frontend/src/advisor/AdvisorView.vue
@@ -98,7 +98,7 @@ export default {
             return result.name + " ("  + result.email  + ")"; 
         },
         handleSubmit(result) {
-            axios.get(`/user/advisor/student/${result.student_ID}/taken`)
+            axios.get("/user/advisor/student/${result.student_ID}/taken")
             .then(response =>{
             var obj = response.data[0]; 
             this. coursesTaken= Object.keys(obj).map(key => obj[key]);
@@ -106,8 +106,8 @@ export default {
             .catch(error =>{
                 console.log(error)
             }),
-            
-            axios.get(`/user/advisor/student/${result.student_ID}/recommended`)
+
+            axios.get("/user/advisor/student/${result.student_ID}/recommended")
             .then(response =>{
             var obj = response.data[0]; 
             this. coursesRecommended= Object.keys(obj).map(key => obj[key]);
