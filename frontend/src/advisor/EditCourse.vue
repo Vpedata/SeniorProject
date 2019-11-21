9b3b1e1bcdaf0c75b5148e7041d1efb9ed67b3e1
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-20 22:18:16 -0500

    Fixing edit button

--- frontend/src/advisor/EditCourse.vue
+++ frontend/src/advisor/EditCourse.vue
@@ -1,11 +1,22 @@
 <template>
     <v-dialog max-width="1000px">
-        <v-btn flat slot="activator" class="success">Edit Course</v-btn>
+        <template v-slot:activator="{ on }">
+        <v-btn flat class="success">Edit Course</v-btn>
+        </template>
         <v-card>
             <v-card-title>
                 <h2>Edit Course</h2>
             </v-card-title>
-            
+            <v-card-text>
+                <v-form class="px=3">
+                    <v-text-field label="Course Name" v-model='course_name'></v-text-field>
+                    <v-text-field label="Course Description" v-model='course_desc'></v-text-field>
+                    <v-checkbox v-model='isCore' label="Core Course"></v-checkbox>
+                    <v-text-field  v-model = 'courseCode' label = 'Course Code' single-line type="number" hide-details></v-text-field>
+                    <v-text-field  v-model = 'class_credit' label = 'Credits' single-line type="number" hide-details></v-text-field>
+                    <v-btn flat class="success mx-0 mt-3">Create Course </v-btn>
+                </v-form>
+            </v-card-text>
         </v-card>
     </v-dialog> 
 </template>
