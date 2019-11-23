8c6e5764cde4226c3580a5114017fa3cd83c5d69
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-22 19:08:15 -0500

    Adding list items as course

--- frontend/src/advisor/classListComponentAdvisor.vue
+++ frontend/src/advisor/classListComponentAdvisor.vue
@@ -1,35 +1,30 @@
-
 <template>
-        <v-list-item>
+    <v-list-item @click.stop="dialog = true">
             <v-list-item-content>
-                    <v-btn color="amber darken-1" dark @click.stop="dialog = true">
-                        <span>{{course.name}}</span>
-                        <v-icon>{{ icons.mdiPencil }}</v-icon>
-                        <div class="mx-2"></div>
-                        <v-icon>{{ icons.mdiDelete}}</v-icon>
-                    </v-btn>
-                    <v-dialog v-model="dialog" width="500">
-                    <v-card>
-                        <v-card-title
-                            class="headline grey lighten-2"
-                            primary-title
-                        >
+                <v-list-item-title>{{course.name}}</v-list-item-title>
+                <v-card>
+                    <v-card-title class="headline grey lighten-2" primary-title>
                          {{course.name}}
-                        </v-card-title>
-                        <v-divider></v-divider>
-                        <v-card-text>
-                            Course Code: {{course.courseCode}}, Credit Hours: {{course.creditHours}}
-                        </v-card-text>
-                        <v-divider></v-divider>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue" text @click="dialog = false">
-                                Ok
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
+                    </v-card-title>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        Course Code: {{course.courseCode}}, Credit Hours: {{course.creditHours}}
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue" text @click="dialog = false">Ok</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-list-item-content>
+             <v-list-item-action>
+                <v-btn color="primary" depressed>
+                    <v-icon left>{{ icons.mdiDelete }}</v-icon>
+                </v-btn>
+                 <v-btn color="primary" depressed>
+                    <v-icon left>{{ icons.mdiDelete }}</v-icon>
+                </v-btn>
+        </v-list-item-action>
         </v-list-item> 
 </template>
 
