523084b0a7989195bffd1e31ba240201f9b238ec
jpk1101 <jpk.mymail@gmail.com>
2019-11-23 16:52:04 -0500

    StudentAddClassPage Overhaul
    Overhauled the StudentAddClassPage to be more like the ClassList design. Created a unique class list component for this page. This component includes a button that will be able to add a corresponding course to the list of recommended courses.

--- frontend/src/studentpages/StudentAddClassPage.vue
+++ frontend/src/studentpages/StudentAddClassPage.vue
@@ -9,7 +9,8 @@
                 </v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-toolbar-items>
-                    <v-btn  @click="$router.push('/fe/student')" dark>Home</v-btn>
+                    <v-btn  @click="$router.push('/fe/adv/createcourse')" dark>Create Course</v-btn>
+                    <v-btn  @click="$router.push('/fe/adv/advisor')" dark>Home</v-btn>
                     <v-btn  @click="$router.push('/messages')" dark>Messages</v-btn>
                     <v-btn  @click="logout" dark>Logout</v-btn>
                 </v-toolbar-items>
@@ -19,65 +20,13 @@
                 <v-col cols="3"></v-col>
                 <v-col cols="9" lg="6">
                 <v-card class="mt-n16 mx-auto" elevation="12" height="600px">
-                    <v-toolbar flat>                                <!--header of list box-->
-                        <v-toolbar-title class="grey--text">Add Classes (Only Available Classes List)</v-toolbar-title>
-                    </v-toolbar>                                    <!---->
-                    <p>List: {{ initial_courses }}</p>              <!--What i want to send when finished-->
-
+                    <v-toolbar flat>
+                        <v-toolbar-title class="grey--text">Class List</v-toolbar-title>
+                    </v-toolbar>
                     <v-list style="max-height: 600px" class="overflow-y-auto">
-                        <classComponent v-for="course in currentReccSemester" :course="course" :key="course.course_ID"/> 
-                    </v-list> 
-
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-dialog v-model="dialog" width="500">
-                                    <template v-slot:activator="{ on }">
-                                    <v-btn color="amber darken-1" dark v-on="on">
-                                        GetClassName
-                                    </v-btn>
-                                    </template>
-                                    <v-card>
-                                        <v-card-title
-                                            class="headline grey lighten-2"
-                                            primary-title
-                                        >
-                                            GetClassName
-                                        </v-card-title>
-                                        <v-divider></v-divider>
-                                        <v-card-text>
-                                            GetClassSummary
-                                        </v-card-text>
-                                        <v-divider></v-divider>
-                                        <v-card-actions>
-                                            <v-spacer></v-spacer>
-                                            <v-btn color="blue" text @click="dialog = false">
-                                                Add Class
-                                            </v-btn>
-                                            <v-btn color="red" text @click="dialog = false">
-                                                Cancel
-                                            </v-btn>
-                                        </v-card-actions>
-                                    </v-card>
-                                </v-dialog>
-                            </v-list-item-content>
-                        </v-list-item>
-
-                        
-            
-                        <v-list-item @click="$router.push('/classlist')">
-                            <v-list-item-content>
-                            <v-list-item-title>Repeat...</v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                    
+                        <classComponent v-for="course in courses" :course="course" :key="course.course_ID"/> 
+                    </v-list>                  
                 </v-card>
-                </v-col>
-  
-            </v-row>
-            <v-row>
-                <v-col cols="8">
-                    <v-btn class="mt-12 ma-12" outlined color="blue" @click="$router.push('/fe/student')" dark>Back</v-btn>
                 </v-col>
             </v-row>
         </div>
@@ -88,7 +37,8 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
-
+import classComponent from './classListComponentAdd.vue'
+import axios from 'axios'
 export default {
     computed: {
         ...mapState({
@@ -97,57 +47,42 @@ export default {
     }, 
     data: () => ({
         dialog: false,
-        initial_courses: [],
-        final_courses: [],
-        currentReccSemester: JSON,
-        name: " "
-  }),
-  
-    methods: {
-    logout: function() {
-      axios
-        .get("/auth/logout")
-        .then(respone => {
-          this.$router.push("/");
-        })
-        .catch(err => {
-          console.log(err);
-        });
+        name: " ",
+        courses: JSON
+    }),
+
+    components: {
+        classComponent,
     },
-    generateDefaults: function() {
-      axios
-        .get("/user/student/courses/recommendedCourses")
-        .then(response => {
-          var obj = response.data[0];                               
-          var allCourses = Object.keys(obj).map(key => obj[key]);   //literally all courses
-          var selectedCourses = [];
-          var numOfReccCourses = allCourses.length;                               
-          var i = 0;
-          while (i < numOfReccCourses) {
-            console.info(allCourses[i]);
-            selectedCourses.push(allCourses[i]);                    
-            console.info(selectedCourses);
-            i++;
-          }
-
-          this.courses = selectedCourses;
-          console.info(this.courses)
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  },
-
+    methods: {
+        logout: function () {
+            axios.get("/auth/logout").then(response =>{
+                this.$router.push('/');
+            }).catch(err =>{
+                console.log(err);
+            });
+        }
+    },
     beforeMount(){
       axios
       .get('/user/getName')
       .then(response => {
         this.name = response.data.firstName + " " + response.data.lastName;
-      }).bind(this)
+      })
       .catch(error => {
         console.log(error)
+      });
+
+      axios.get('/course/student/yetToTake')
+      .then(response =>{
+         var obj = response.data[0]; 
+         this.courses = Object.keys(obj).map(key => obj[key]);
+         console.info(this.courses);
+      })
+      .catch(error =>{
+          console.log(error)
       })
   }
 };
 </script>
+
---
+++ frontend/src/studentpages/classListComponentAdd.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-list-item>
+            <v-list-item-content @click.stop="dialog = true">
+                <v-list-item-title>{{course.name}}</v-list-item-title>
+                <v-dialog v-model="dialog" width="500">
+                <v-card>
+                    <v-card-title class="headline grey lighten-2" primary-title>
+                         {{course.name}}
+                    </v-card-title>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        Course Code: {{course.courseCode}}, Course Description: {{course.description}},Credit Hours: {{course.creditHours}}
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue" text @click="dialog = false">Ok</v-btn>
+                    </v-card-actions>
+                </v-card>
+                </v-dialog>
+            </v-list-item-content>
+            <v-list-item-action>
+                    <v-row>
+                    <v-btn class="mx-2" fab dark small color="green" >
+                        <v-icon dark>mdi-add</v-icon>
+                    </v-btn>
+                    </v-row>
+            </v-list-item-action>
+        </v-list-item> 
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: "classComponent",
+    props: {
+        course: Object,
+    },
+    data: () => ({
+        dialog: false,
+        name: ""
+    }),
+    methods: {
+      addRecCourse: function() {
+          /*
+        axios.post("/user/student/course/addRecCourse", {
+                ??
+          }).then(function (response) {
+              console.log(response);
+          })
+          .catch(function (error) {
+              console.log(error);
+          });
+          */
+        }   
+    },
+    beforeMount(){
+      axios
+      .get('/user/getName')
+      .then(response => {
+        this.name = response.data.firstName + " " + response.data.lastName;
+      }).bind(this)
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
