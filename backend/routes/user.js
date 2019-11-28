fb5a50e2dece63ce46accb29ee863864569b9302
Deep Desai <44065733+desaid64@users.noreply.github.com>
2019-11-27 21:09:43 -0500

    removing uneeded routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -53,35 +53,5 @@ router.get("/:id",authMiddleware, (req, res, next) => {
   })
 });
 
-/**
-//Delete an user
-router.delete("/:id", (req, res, next) => {
-  var sql = "DELETE FROM User WHERE user_ID = ?";
-  db.query(sql, [req.params.id], (err, rows, fields) => {
-    if (err) throw err;
-    res.json({'status': 'success'});
-  })
-});
 
-//Insert an user
-router.post("/", (req, res, next) => {
-  let user = req.body.user;
-  var sql = "INSERT INTO User SET ? ";
-  db.query(sql, { user: user }, (err, rows, fields) => {
-    if (err) throw err;
-    res.json({'status': 'success'});
-  })
-});
-
-//Update an user
-router.put("/", (req, res, next) => {
-  let user_ID = req.body.user_ID;
-  let user = req.body.user;
-  var sql = "UPDATE User SET user = ? WHERE user_ID= ?";
-  db.query(sql, [user, user_ID], (err, rows, fields) => {
-    if (err) throw err; 
-    res.json({'status': 'success'});
-  })
-});
-*/
 module.exports = router; 
