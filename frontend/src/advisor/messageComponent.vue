f9568118b9f8100e7e8998202ee3f28f2f7d7f88
Josiah Bell <bellj5@students.rowan.edu>
2019-11-30 16:04:41 -0500

    Styling for Advisor Messages
    Hopefully this works smoothly

--- frontend/src/advisor/messageComponent.vue
+++ frontend/src/advisor/messageComponent.vue
@@ -1,9 +1,12 @@
 <template>
-    <v-list-item>
+    <v-list-item :class="message{{message.isFromUser ? (message.reciever == this.userID ? ' received' : ' sent') : ' system'}}">
             <v-list-item-content>
                 <v-list-item-title>
                     <span :class="classObject">
-                        <small>{{message.user}}</small>:{{message.message}}
+                        <small>{{message.user}}</small>
+                        <div :class="text">
+                            :{{message.message}}
+                        </div>
                     </span>
                 </v-list-item-title>
             </v-list-item-content>
@@ -18,7 +21,16 @@ export default {
     props: {
         message: Object,
     },
-    created: function() {
+    data() {
+        return {
+            userID: -1,
+        }
+    },
+    beforeMount() {
+        axios.get('/user/getmyid')
+             .then(response => {
+                this.userID = response.data;
+              });
     },
     computed: {
         classObject: function() {
@@ -31,9 +43,49 @@ export default {
         }
 
     },
-    data: function() {
-        return {
-        } 
-    }
 }
 </script>
+
+<style>
+.message {
+    margin: 10px 0;
+    width: 100%;
+}
+
+.text {
+    max-width: 300px;
+    border-radius: 5px;
+    padding: 12px;
+    display: inline-block;
+}
+
+.received {
+    text-align: left;
+    
+    .text {
+        background: #602e00;
+        color: #ffffff;
+    }
+}
+
+.sent {
+    text-align: right;
+    
+    .text {
+        background: #ffee00;
+    }
+}
+
+.system {
+    text-align: center;
+    border-top-style: solid;
+    border-bottom-style: solid;
+    border-color: #000000;
+    border-width: 2px;
+
+    .text {
+        background: transparent;
+        color: #555555;
+    }
+}
+</style>
