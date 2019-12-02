d4e05945005a3b95c35a08e177e94827babed7cd
jpk1101 <jpk.mymail@gmail.com>
2019-12-01 21:05:14 -0500

    EditCourse update_completed()
    Testing axios call

--- frontend/src/studentpages/StudentEditCompleted.vue
+++ frontend/src/studentpages/StudentEditCompleted.vue
@@ -72,30 +72,33 @@ export default {
             let temp_index = 0
             this.update_courses = ""
             this.update_grades = ""
-            let avail_parse = JSON.parse(JSON.stringify(this.avail_courses))
-            for (let i=0;i<avail_courses.length;i++){
-                let check_course = avail_parse[i].name
-                let check_grade = avail_parse[i].grade
+            for (var i=0 ;i<this.avail_courses.length; i++){
+                let check_course = this.avail_courses[i].name
+                let check_grade = this.avail_courses[i].grade
                 if (check_grade != '') {
-                    this.update_courses.concat(toString(check_course) + ",")
-                    this.update_grades.concat(toString(check_grade) + ",")
+                    this.update_courses = update_courses + check_course + ","
+                    this.update_grades = update_grades + check_grade + ","
                     temp_index ++
                 }
             }
             update_courses = grade_str.substring(0, grade_str.length - 1);
             update_grades = grade_str.substring(0, grade_str.length - 1);
+            
+            console.info(update_courses)
+            console.info(update_grades)
+            
+            /*
             axios.post("/user/student/courses/taken", {
                 classes: this.update_courses,
                 student_ID: this.id,
                 grades: this.update_grades
-          }).then(function (response) {
+            }).then(function (response) {
               console.log(response);
-          })
-          .catch(function (error) {
+            })
+            .catch(function (error) {
               console.log(error);
-          })
-            $router.push('/fe/student')
-            
+            })
+            */
         } 
     },
     beforeMount(){
